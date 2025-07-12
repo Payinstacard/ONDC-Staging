a92b11ef7963a8c8402c3185fefc5a9a7ec660e9
Venkatesh S M <venkatesh@skygoalnext.com>
2025-07-12 12:58:41 +0530

    updated code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv/config.js");
 const signedPrivateKey = process.env.SIGNING_PRIVATE_KEY;
 const express = require("express");
-const { signMessage, sharedKey } = require("./utils/ondc.js");
+const { signMessage, sharedKey, decryptAES256ECB } = require("./utils/ondc.js");
 const morgan = require("morgan");
 const app = express();
 const { v4: uuidv4 } = require("uuid");
@@ -11,7 +11,7 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/health", (req, res) => res.send("Health OK!!"));
 
-app.post("/on_subscribe", function (req, res) {
+app.post("/on_subscribe/on_subscribe", function (req, res) {
   const { challenge } = req.body;
   console.log("ON SUBSCRIBE", challenge);
   const answer = decryptAES256ECB(sharedKey, challenge);
@@ -33,7 +33,7 @@ const htmlFile = `
 `;
 
 app.get("/ondc-site-verification.html", async (req, res) => {
-  const requestId = uuidv4();
+  const requestId = "cc386042-01ce-42f5-b32d-5c5f7019f68e";
   console.log("REQUEST ID", requestId);
   const signedContent = await signMessage(requestId, signedPrivateKey);
   console.log(signedContent);
